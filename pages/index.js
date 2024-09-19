2c1dfb6ab83cf4c65dcdaa86f152a076be2ed952
jebsonHope <90716904+jebsonHope@users.noreply.github.com>
2024-09-19 02:46:34 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>Parabéns você foi sorteado no Familhão e acaba de ganhar R$ 1.000.000 💸💸💸</h1>
+  return (
+    <h1>
+      Parabéns você foi sorteado no Familhão e acaba de ganhar R$ 1.000.000
+      💸💸💸
+    </h1>
+  );
 }
 
 export default Home;
